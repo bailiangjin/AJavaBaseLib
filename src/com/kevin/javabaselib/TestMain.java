ad245ca54f908a5a55f451ff6b05a09f0ca12cbd
bailiangjin <bailiangjin@qq.com>
2016-04-12 21:24:09 +0800

    时间工具类 添加获取具体时间方法

--- src/com/kevin/javabaselib/TestMain.java
+++ src/com/kevin/javabaselib/TestMain.java
@@ -1,34 +1,36 @@
 package com.kevin.javabaselib;
 
-public class TestMain
-{
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		// String gmail = "3b32cdfe@sin1a0000000.com.com.cm";
-		//
-		// System.out.println(isEmailFormat(gmail));
-		// String number = "010-20332058-0000";
-		// System.out.println(FormatCheckUtils.checkPhoneNumberValid(number));
-		// String mobile = "11630016890";
-		// System.out.println(ValidateUtils.checkMobileNumberValid(mobile));
-		// regextest();
+import com.kevin.javabaselib.utils.DateUtils;
+
+import java.util.Date;
+
+public class TestMain {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // String gmail = "3b32cdfe@sin1a0000000.com.com.cm";
+        //
+        // System.out.println(isEmailFormat(gmail));
+        // String number = "010-20332058-0000";
+        // System.out.println(FormatCheckUtils.checkPhoneNumberValid(number));
+        // String mobile = "11630016890";
+        // System.out.println(ValidateUtils.checkMobileNumberValid(mobile));
+        // regextest();
 
 //		GsonTestDemo.testGson();
+        Date date = DateUtils.getDateFromEnString("2016-4-12 21:12:00");
 
-		long sum = 0;
-		long fac = 1;
-		for (int i = 1; i <= 20; i++)
-		{
-			fac = fac * i;
-			System.out.println(fac);
-			sum += fac;
-		}
-		System.out.println(sum);
-		System.out.println("-200".equals(null));
-
-	}
+        System.out.println("date:" + date.getTime());
+        System.out.println("date:" + DateUtils.getLongCurrentDate());
+//        long sum = 0;
+//        long fac = 1;
+//        for (int i = 1; i <= 20; i++) {
+//            fac = fac * i;
+//            System.out.println(fac);
+//            sum += fac;
+//        }
+//        System.out.println(sum);
+    }
 
 }
